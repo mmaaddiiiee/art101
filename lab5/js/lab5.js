501455053234fa2208fb69c134d7c993058cc276
mmaaddiiiee <madelyn.sutherland2002@gmail.com>
2022-10-17 12:44:56 -0700

    lab 5
    github upload

---
+++ lab5/js/lab5.js
@@ -0,0 +1,20 @@
+/**
+ * Author:    Madelyn
+ * Created:   10.16.2022.
+ **/
+
+ //Define Variables
+ make = "Honda";
+ model = "Accord";
+ color = "Blue Pearl";
+ year = 2005;
+
+ //calculate
+ age = 2022 - year;
+
+ //output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
