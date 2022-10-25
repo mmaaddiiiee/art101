4172c98edf99ff3d9d542d2bf5a3b12eb81f2266
mmaaddiiiee <madelyn.sutherland2002@gmail.com>
2022-10-24 21:50:47 -0700

    lab 7 file structure
    main site upload

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+// Lab 4. Step 3 and 4.
+// Pseudocoding and Problem-solving.
+// @link   URL
+// @file   This files defines the MyClass class.
+// @Madelyn Sutherland and Emma Radichkova.
+// @since  10.12.2022
+//
+//
+//User inputs a letter
+//Computer denies or accepts letter
+  //Removes letter from list OR fills it in blank
+//Tests win/lose condition
+  //Win condition: Player guesses word before 9th letter
+  //Lose condition: Player takes over 9 turns to get word
+//Repeat step 1-3
